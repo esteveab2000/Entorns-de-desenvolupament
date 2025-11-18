37c62d412259651a28054c4cbd11fd6631e22b3b
esteveab2000 <esteve.amargant25@lacetania.cat>
2025-11-18 20:49:51 +0100

    Update ciutats.py
    Canvi per el pull request

--- ciutats.py
+++ ciutats.py
@@ -1,4 +1,5 @@
 ciutats = {
     "Manresa": {"codi_postal": 08243, "habitants": 27, "altura": 777},
     "Barcelona": {"codi_postal": 8001, "habitants": 1536790, "altura": 0}
+    "Sahara": {"codi_postal": 1234, "habitants": 1536795, "altura": 100}
 }
